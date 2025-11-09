b7defb785d0e9c0495d15536c52f684c996824a6
Sejjusa <sejjtechnologies@gmail.com>
2025-11-09 20:58:29 +0300

    Add PWA support: resized icons, install screenshots, service worker, and updated home.html

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,20 +1,41 @@
+const CACHE_NAME = 'supermarket-cache-v1';
+const ASSETS = [
+  '/',
+  '/static/icons/icon1.png',
+  '/static/icons/icon2.png',
+  '/static/icons/icon3.png'
+];
+
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Activate immediately
   event.waitUntil(
-    caches.open('supermarket-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/static/icons/icon1.png',
-        '/static/icons/icon2.png',
-        '/static/icons/icon3.png'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
     })
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim(); // Take control of all pages
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() =>
+        caches.match('/')
+      );
     })
   );
 });
